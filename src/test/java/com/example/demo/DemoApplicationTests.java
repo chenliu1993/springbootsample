4d6d2bbab0c06a65032ba90c017aaf5d83b6569c
chenliu1993 <lc17319031387@163.com>
2023-01-12 12:47:33 +0000

    Add a simple fake test which actually test nothing
    TODO
    1. Add more throws
    2. How to test with sql
    
    Signed-off-by: chenliu1993 <lc17319031387@163.com>

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,47 +2,45 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+// import org.springframework.aop.interceptor.ExposeInvocationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.demo.service.PostService;
 
-import com.example.demo.config.AspectConfig;
-import com.example.demo.service.UserService;
+import com.example.demo.domain.PostEntity;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+// import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 	
 	// @Autowired
-	// private UserService demoItemService;
+	// private UserService demoUserService;
 
-	// @Autowired
-	// private Item demoItem;
+	@Autowired
+	private PostService demoPostService;
 
-	// @Test
-	// public void TestLogAspect() {
-	// 	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AspectConfig.class);
-    //     // demoItemService = context.getBean(ItemService.class);
-	// 	// Item demoItem = context.getBean(Item.class);
-	// 	demoItem.setId((long)10);
-	// 	demoItem.setName("Test Phone");
-	// 	demoItem.setPrice((float)1200.0);
-	// 	demoItem.setVendor("This is for test");
 
-	// 	try {
-	// 		demoItemService.save(demoItem);
-	// 	} catch (Exception e) {
-	// 		e.printStackTrace();
-	// 	}
-		
- 
-    //     System.out.println("=======================================");
-    //     demoItemService.findOne((long) 10);
- 
-    //     context.close();
-	// }
+
+	@Test
+	public void TestPostSave() {
+		// AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AspectConfig.class);
+        // demoPostService = context.getBean(PostService.class);
+
+
+		// This is the demo postEntity
+		PostEntity testPostEntity = new PostEntity();
+		testPostEntity.setAuthor("dummy");
+		testPostEntity.setContent("why are you looking?");
+		testPostEntity.setId(Long.valueOf(1000));
+		testPostEntity.setTheme("Japanese is too hard to learn!!");
+
+		demoPostService.save(testPostEntity);
+
+		System.out.println("=======================================");
+	}
 	public static void main(String[] args)
     {
 		System.out.println("test");
